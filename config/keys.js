43f1b87b1a836b2d1bd4458686becf235a16fa2e
Aparna Perwala <aparna.perwala@gmail.com>
2019-01-27 14:36:49 -0800

    Added JWT token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb://aparnaperwala:aparna1234@ds041177.mlab.com:41177/devlinked'
+  mongoURI: 'mongodb://aparnaperwala:aparna1234@ds041177.mlab.com:41177/devlinked',
+secretOrKey: 'secret'
+
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require('../../models/Users');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // @route GET api/users/test
 // @desc  Tests user route
@@ -68,7 +70,26 @@ router.post('/login', (req,res) => {
       bcrypt.compare(password, user.password)
       .then(isMatch =>{
         if (isMatch){
-          res.json({msg: 'Success'});
+          //User matched
+          const payload = {
+            id: user.id,
+            name: user.name,
+            avatar: user.avatar
+          };
+          //Sign token
+          jwt.sign(
+            payload, 
+            keys.secretOrKey,
+            {expiresIn: 3600},
+            (err, token) => {
+              res.json(
+                {
+                  success: true,
+                  token: 'Bearer ' + token
+                }
+              )
+            }
+            );
         } else {
           return res.status(400).json({email: 'password incorrect'});
         }
